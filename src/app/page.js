be1e0614e7a3937d9a99dc156ae296941342d397
Karina Savoie <karinasavoie@Karinas-MacBook-Pro.local>
2025-05-27 17:28:02 +0100

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -29,8 +29,8 @@ export default function Home() {
                 LEARN MORE <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
-            <h1 className="text-[52px] text-white leading-[60px] md:text-title text-center font-bold w-full md:w-[90%] lg:w-[70%] xl:w-[65%] 2xl:w-[50%] xl:text-[55px] xl:leading-[70px]">
-              What if your ops, data, and decisions all spoke the same language?
+            <h1 className="text-[52px] text-white leading-[60px] md:text-title text-center font-bold w-full md:w-[90%] lg:w-[70%] xl:w-[70%] 2xl:w-[60%] xl:text-[55px] xl:leading-[70px]">
+              What if your data, and decisions all spoke the same language?
             </h1>
             <h2 className="text-[25px] text-white text-center font-extralight w-full md:w-[90%] lg:w-[50%] xl:w-[50%] 2xl:w-[50%] xl:text-[30px]">
               We organize the mess, map the strategy, and build systems that
